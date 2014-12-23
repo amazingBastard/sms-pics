cd917687fcc91aca185e7b70458f7b31f084254c
Damir <damir@withpulp.com>
2014-12-23 00:38:37 -0800

    styled header and footer

--- client/lib/constants.js
+++ client/lib/constants.js
@@ -5,6 +5,6 @@ if (Meteor.App) {
 }
 
 Meteor.App = {
-  NAME: 'Meteor Boilerplate',
-  DESCRIPTION: 'A boilerplate for meteorjs projects http://matteodem.github.io/meteor-boilerplate/'
+  NAME: 'Snich',
+  DESCRIPTION: 'Share photos, videos, and sound bites anonymously via sms text message.'
 };
