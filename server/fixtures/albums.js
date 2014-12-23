2ba394259276ff35761810a01e8d0b890dc89e18
Damir <damir@withpulp.com>
2014-12-23 13:54:27 -0800

    added albums fixtures and placeholder images, created albums collection and publications

---
+++ server/fixtures/albums.js
@@ -0,0 +1,29 @@
+// The "||" notation doesn't work yet
+Fixtures = typeof Fixtures !== "undefined" ? Fixtures : {};
+
+Fixtures.albums = [
+  {
+    'img': '/images/albums/space/placeholder01.jpg',
+    'category': 'space'
+  },
+  {
+    'img': '/images/albums/space/placeholder02.jpg',
+    'category': 'space'
+  },
+  {
+    'img': '/images/albums/space/placeholder03.jpg',
+    'category': 'space'
+  },
+  {
+    'img': '/images/albums/puppies/placeholder01.jpg',
+    'category': 'puppies'
+  },
+  {
+    'img': '/images/albums/puppies/placeholder02.jpg',
+    'category': 'puppies'
+  },
+  {
+    'img': '/images/albums/puppies/placeholder03.jpg',
+    'category': 'puppies'
+  }
+];
