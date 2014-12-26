dbf47ba838c955420a3a32882b137246f88a11f9
Damir <damir@withpulp.com>
2014-12-25 20:32:06 -0800

    added npm package with twilio, added velocity karma and html reporter - some issues with html reporter

--- server/config/security.js
+++ server/config/security.js
@@ -1,3 +1,2 @@
 BrowserPolicy.content.allowOriginForAll("http://fonts.googleapis.com");
 BrowserPolicy.content.allowOriginForAll("http://fonts.gstatic.com");
-BrowserPolicy.content.allowOriginForAll("http://maxcdn.bootstrapcdn.com");
